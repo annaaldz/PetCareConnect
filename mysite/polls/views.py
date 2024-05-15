f26c99e48219d89cf7a9555a5d3cec4903372df4
Anna Carolina <anna.souza51@etec.sp.gov.br>
2024-05-15 11:06:42 -0300

    atualizando

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
 
+# Create your viewimport requests
+from django.shortcuts import render
+from django.http import JsonResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-# Create your views here.
+def petCare_view(request):
+    response = request.get('https://api-petcare.onrender.com/employees?auth=GciOiJIUzI1NiIsInR5cCI6I')
+    data = response.json()
+    return JsonResponse(data)
